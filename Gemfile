3ca99ca7a36e5ad01351a2df72d7557bfea3ec8c
Alex Erling <alex.erling@gmail.com>
2017-12-15 19:53:48 +0000

    Added page pagination

--- Gemfile
+++ Gemfile
@@ -19,6 +19,7 @@ gem 'ckeditor'
 gem 'paperclip'
 gem 'acts-as-taggable-on', '~> 4.0'
 gem 'friendly_id', '~> 5.1.0'
+gem 'will_paginate', '~> 3.0.7'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,9 @@
 class PostsController < ApplicationController
   def index
     if params[:tag]
-      @posts = Post.tagged_with(params[:tag])
+      @posts = Post.tagged_with(params[:tag]).order("created_at desc").paginate(page: params[:page], per_page: 10)
     else
-      @posts = Post.all
+      @posts = Post.all.order("created_at desc").paginate(page: params[:page], per_page: 10)
     end
   end
 
